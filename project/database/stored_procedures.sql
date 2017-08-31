2d328a7e7281456e9e16b626bca8f695499fc273
Konrad Gawlinski <konrad.gawlinski@gmail.com>
2017-08-31 23:20:40 +0200

    Implement create/update product stored procedure

--- project/database/stored_procedures.sql
+++ project/database/stored_procedures.sql
@@ -18,3 +18,55 @@ RETURNING 1;
 $$
 LANGUAGE SQL;
 
+CREATE OR REPLACE FUNCTION create_product(
+  _product_id INTEGER, _sku VARCHAR, _type VARCHAR, _properties JSONB)
+  RETURNS INTEGER AS
+$$
+    var createInputLists = (properties) => {
+        var columns = [],
+            values = [];
+
+        for (var column of Object.keys(properties)) {
+            var value = JSON.stringify(properties[column]).replace(/'/g,"\\'");
+
+            values.push(`'${value}'`);
+            columns.push(column);
+        }
+
+        return [
+          columns.join(), values.join()
+        ];
+    };
+
+    var inputLists = createInputLists(_properties),
+        sku = _sku ? `'${_sku}'` : null,
+        type = _type ? `'${_type}'` : null;
+
+    return plv8.execute(
+        `INSERT INTO catalog.product_entity(id, sku, type, ${inputLists[0]})
+            VALUES (${_product_id}, ${sku}, ${type}, ${inputLists[1]});`);
+$$
+LANGUAGE PLV8;
+
+CREATE OR REPLACE FUNCTION update_product(_product_id INTEGER, _properties JSONB) RETURNS TEXT AS
+$$
+    var createUpdateStatements = (properties) => {
+        var updateStatements = [];
+
+        for (var column of Object.keys(properties)) {
+            var value = JSON.stringify(properties[column]).replace(/'/g,"\\'");
+
+            updateStatements.push(`${column} = ${column} || '${value}'`);
+        }
+
+        return updateStatements.join();
+    };
+
+    var updateStatements = createUpdateStatements(_properties);
+
+    return plv8.execute(`UPDATE product_entity SET ${updateStatements} WHERE id=${_product_id};`);
+$$
+LANGUAGE PLV8;
+
+--SELECT create_product(99999993, 'abc', 'Config', '{"de_de":{"text":"value"}}');
+--SELECT update_product(99999993, '{"de_de":{"mytext":"his value"}}');
