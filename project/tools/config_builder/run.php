b4443909600b66fa2d5f02bb67ef9845a6937e85
Konrad Gawlinski <konrad.gawlinski@gmail.com>
2018-10-02 23:22:12 +0200

    create environment specific configuration

--- project/tools/config_builder/run.php
+++ project/tools/config_builder/run.php
@@ -3,13 +3,14 @@
 use Symfony\Component\Yaml\Yaml;
 
 define('APPLICATION_ROOT', __DIR__.'/../../');
+define('APP_ENV', getenv('APP_ENV'));
 
 /** @var \Composer\Autoload\ClassLoader() $loader */
 $loader = require_once APPLICATION_ROOT.'vendor/autoload.php';
 $loader->addPsr4('Nu3\\Configuration\\', APPLICATION_ROOT.'tools/config_builder');
 
 $configDefault = Yaml::parse(file_get_contents(APPLICATION_ROOT.'config/default.yml'));
-$configLocal = Yaml::parse(file_get_contents(APPLICATION_ROOT.'config/local.yml'));
+$configLocal = Yaml::parse(file_get_contents(APPLICATION_ROOT.'config/'.APP_ENV.'.yml"));
 
 $configProcessor = new \Nu3\Configuration\Processor();
 $config = var_export($configProcessor->run([$configDefault, $configLocal]), true);
